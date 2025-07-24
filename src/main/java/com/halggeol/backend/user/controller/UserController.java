343410c55e573b71899d58ca502d2837dc09f3c4
eunwoo <lkjin0110@gmail.com>
2025-07-24 14:16:05 +0900

    fix: join HTTP Method 수정

--- src/main/java/com/halggeol/backend/user/controller/UserController.java
+++ src/main/java/com/halggeol/backend/user/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class UserController {
         return ResponseEntity.status(userService.requestJoin(user)).build();
     }
 
-    @GetMapping("")
+    @PostMapping("")
     public ResponseEntity<Void> join(UserJoinDTO user, @RequestParam String token) {
         return ResponseEntity.status(userService.join(user, token)).build();
     }
