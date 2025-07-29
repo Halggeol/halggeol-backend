b0d22267c4845e41e2ea32f9c209cc3c20de42fb
eunwoo <lkjin0110@gmail.com>
2025-07-29 11:30:54 +0900

    fix: 전화번호 정규식 수정

--- src/main/java/com/halggeol/backend/security/util/RegexConstants.java
+++ src/main/java/com/halggeol/backend/security/util/RegexConstants.java
@@ -2,7 +2,7 @@ package com.halggeol.backend.security.util;
 
 public class RegexConstants {
     // 01012345678
-    public static final String PHONE_PATTERN = "^01[016789]\\d{3,4}\\d{4}$";
+    public static final String PHONE_PATTERN = "^01[016789]\\d{8}$";
 
     // 최소 8자 이상의 영문 대/소문자, 숫자, 특수문자로 구성
     public static final String PASSWORD_PATTERN
