bdaea7f83fe877d9762100d59d7c643f699ef226
eunwoo <lkjin0110@gmail.com>
2025-07-31 16:25:51 +0900

    fix: 금융 이해도 설문 관련 DTO 유효성 검사 어노테이션 추가

--- src/main/java/com/halggeol/backend/user/dto/KnowledgeAnswerDTO.java
+++ src/main/java/com/halggeol/backend/user/dto/KnowledgeAnswerDTO.java
@@ -1,16 +1,25 @@
 package com.halggeol.backend.user.dto;
 
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class KnowledgeAnswerDTO {
-    private int number;
-    private int score;
-    private boolean correct;
+    @NotNull
+    @Range(min = 0, max = 9)
+    private Integer number;
+
+    @NotNull
+    @Range(min = 1, max = 3)
+    private Integer score;
+
+    @NotNull
+    private Boolean correct;
 }
--- src/main/java/com/halggeol/backend/user/dto/KnowledgeSurveyRequestDTO.java
+++ src/main/java/com/halggeol/backend/user/dto/KnowledgeSurveyRequestDTO.java
@@ -1,7 +1,10 @@
 package com.halggeol.backend.user.dto;
 
 import java.util.List;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class KnowledgeSurveyRequestDTO {
-    @NotEmpty
+    @NotNull
+    @Valid
+    @Size(min = 10, max = 10)
     private List<KnowledgeAnswerDTO> answers;
 }
