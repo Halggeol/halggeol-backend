4d6cf28c133d3a1baf0cff06a876986779752dbe
eunwoo <lkjin0110@gmail.com>
2025-07-31 19:35:13 +0900

    fix: 설문조사 진입점에 따른 분기를 위한 Utility 클래스 구현

--- src/main/java/com/halggeol/backend/security/util/SecurityUtil.java
+++ src/main/java/com/halggeol/backend/security/util/SecurityUtil.java
@@ -1,4 +1,31 @@
 package com.halggeol.backend.security.util;
 
+import com.halggeol.backend.security.domain.CustomUser;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Log4j2
+@Component
 public class SecurityUtil {
+    public static CustomUser getLoggedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null
+            || !auth.isAuthenticated()
+            || auth instanceof AnonymousAuthenticationToken
+        ) {
+            return null;
+        }
+        return (CustomUser) auth.getPrincipal();
+    }
+
+    public String resolveEmail(String email) {
+        CustomUser user = getLoggedUser();
+        return user != null ? user.getUser().getEmail() : email;
+    }
 }
