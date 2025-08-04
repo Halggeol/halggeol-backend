ca2bec0e8370e4eff955a9d0e3bfc87a443cc399
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-08-04 15:08:16 +0900

    refactor: [FFBV-137] Cache 설정 수정

--- src/main/java/com/halggeol/backend/products/service/ProductDetailServiceImpl.java
+++ src/main/java/com/halggeol/backend/products/service/ProductDetailServiceImpl.java
@@ -13,6 +13,7 @@ import com.halggeol.backend.products.mapper.ProductDetailMapper;
 import com.halggeol.backend.recommend.service.RecommendService;
 import com.halggeol.backend.security.domain.CustomUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
