79abf855f646704709ad99fca958459c57e85013
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-08-05 15:51:23 +0900

    style: [FFBV-137] 코드 스타일 수정

--- src/main/java/com/halggeol/backend/recommend/dto/ProductVectorUpdateResponseDTO.java
+++ src/main/java/com/halggeol/backend/recommend/dto/ProductVectorUpdateResponseDTO.java
@@ -1,16 +1,19 @@
 package com.halggeol.backend.recommend.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductVectorResponseDTO {
-    private String id;
-    private String name;
+@Builder
+public class ProductVectorUpdateResponseDTO {
+
+    private String productId;
     private Double yieldScore;
     private Double riskScore;
     private Double costScore;
     private Double liquidityScore;
     private Double complexityScore;
+    private Double algoCode;
 }
