b905270aba52c81a15d8c8db79828e4c6350c9db
Roa Kim <roa5108@gmail.com>
2025-08-12 10:31:11 +0900

    feat: [FFBV-158] 최근 검색어 개별 삭제 및 전체 삭제 API 구현

--- src/main/java/com/halggeol/backend/products/unified/elasticsearch/controller/SearchLogController.java
+++ src/main/java/com/halggeol/backend/products/unified/elasticsearch/controller/SearchLogController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchLogController {
     private final SearchLogService searchLogService;
 
+    // 최근 검색어 조회
     @GetMapping("/recent")
     public ResponseEntity<?> getRecentSearches(
         @AuthenticationPrincipal CustomUser user
@@ -29,6 +32,31 @@ public class SearchLogController {
         return ResponseEntity.ok(searchLogService.getRecentSearches(userId));
     }
 
+    // 최근 검색어 개별 삭제
+    @DeleteMapping("/recent/{keyword}")
+    public ResponseEntity<?> deleteRecentSearch(
+        @PathVariable String keyword,
+        @AuthenticationPrincipal CustomUser user){
+        if (user == null) {
+            return ResponseEntity.ok(List.of());
+        }
+        Integer userId = user.getUser().getId();
+        searchLogService.deleteRecentSearch(keyword, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 최근 검색어 전체 삭제
+    @DeleteMapping("/recent/all")
+    public ResponseEntity<?> deleteAllSearches(@AuthenticationPrincipal CustomUser user){
+        if (user == null) {
+            return ResponseEntity.ok(List.of());
+        }
+        Integer userId = user.getUser().getId();
+        searchLogService.deleteAllRecentSearches(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 인기 검색어 조회
     @GetMapping("/popular")
     public ResponseEntity<?> getPopularSearches(){
         return ResponseEntity.ok(searchLogService.getPopularSearches());
